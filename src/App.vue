36f0f00d954e56cb6874649ff49354f160eaeabf
Samuel <torressam333@gmail.com>
2022-02-11 08:07:10 -0700

    Add base global styling to app

--- src/App.vue
+++ src/App.vue
@@ -2,3 +2,19 @@
   <!-- component matched by the route will render here -->
   <router-view></router-view>
 </template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Poppins', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/pages/developers/DevsList.vue
@@ -0,0 +1,8 @@
+<template>
+  <section>Filter Comp Goes Here</section>
+  <section>DevsList Comp Goes here</section>
+</template>
+
+<script>
+export default {};
+</script>
