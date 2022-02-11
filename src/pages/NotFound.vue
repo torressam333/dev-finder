3aa318ff3fef90b2d35e77bf3335c2e6ddd26efe
Samuel <torressam333@gmail.com>
2022-02-11 07:49:28 -0700

    Create base not found vue file

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Whoops...nothing to see heere</h1>
+    <p>Return to the home page</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style></style>
