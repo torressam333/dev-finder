b5b7162b553f2495c6b0d71cef5226bbbd78fc04
Samuel <torressam333@gmail.com>
2022-02-12 10:12:31 -0700

    Instantiate vuex store

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue';
 import router from './router';
 import App from './App.vue';
+import store from './store/store';
 
 const app = createApp(App);
 
-app.use(router).mount('#app');
+app.use(router).use(store).mount('#app');
---
+++ src/store/store.js
@@ -0,0 +1,8 @@
+/**Parent vuex store file
+ * All modules etc... will be loaded through here
+ */
+import { createStore } from 'vuex';
+
+export default createStore({
+  modules: {},
+});
