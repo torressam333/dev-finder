89e925dec7119967dccb81278a16b646872c0747
Samuel <torressam333@gmail.com>
2022-02-13 09:33:41 -0700

    Begin creating simple getters for our vuex store

--- src/store/modules/developers/getters.js
+++ src/store/modules/developers/getters.js
@@ -1,1 +1,9 @@
-export default {};
+export default {
+  getDevelopers(state) {
+    return state.developers;
+  },
+  hasDevelopers(state) {
+    //Will be used as helper getter (checking truthiness)
+    state.developers && state.developers.length > 0;
+  },
+};
