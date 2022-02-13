d734bdb52d705d706ad64d2469eca069a7048372
Samuel <torressam333@gmail.com>
2022-02-13 09:26:59 -0700

    Implement dummy data to work with for now

--- src/store/modules/developers/main.js
+++ src/store/modules/developers/main.js
@@ -5,7 +5,30 @@ import mutations from './mutations.js';
 
 export default {
   namespaced: true,
-  state() {},
+  state() {
+    return {
+      developers: [
+        {
+          id: 'dev1',
+          firstName: 'Jason',
+          lastName: 'Bourne',
+          areas: ['frontend', 'backend'],
+          description:
+            "I'm Jason and I've worked as a freelance web developer for years. Let me help your company by applying my expertise!",
+          hourlyRate: 35,
+        },
+        {
+          id: 'dev2',
+          firstName: 'Roger',
+          lastName: 'Rabbit',
+          areas: ['backend', 'data science'],
+          description:
+            'I am Roger and as a senior software engineer, I can help your company improve its backend and data processes',
+          hourlyRate: 45,
+        },
+      ],
+    };
+  },
   actions,
   getters,
   mutations,
