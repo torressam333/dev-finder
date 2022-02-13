0c29a765949f25e0511e75704bbf0386dd9e4ece
Samuel <torressam333@gmail.com>
2022-02-13 11:53:05 -0700

    Begin building extracted dev details component

---
+++ src/components/developers/DeveloperDetails.vue
@@ -0,0 +1,15 @@
+<template>
+  <li>
+    <h3>{{ devFullName }}</h3>
+    <h4>${{ devRate }} per hour</h4>
+    <div>
+      <span v-for="area in areas" :key="area">{{ area }}</span>
+    </div>
+    <div class="actions">
+      <router-link to="/developers/dev1/contact">Conact Dev</router-link>
+      <router-link to="/developers/dev1">View Details</router-link>
+    </div>
+  </li>
+</template>
+
+<style scoped></style>
