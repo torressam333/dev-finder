d9ecd6b32e4ccd1a461aca6fa2d5718aa585d4f5
Samuel <torressam333@gmail.com>
2022-02-14 07:15:18 -0700

    Add different stlying to base card

--- src/components/base-components/BaseCard.vue
+++ src/components/base-components/BaseCard.vue
@@ -7,9 +7,13 @@
 <style scoped>
 .card {
   border-radius: 10px;
-  box-shadow: 0 3px 8px rgba(0, 0, 0, 0.3);
+  box-shadow: 0 3px 8px rgb(0, 0, 0, 0.6);
   margin: 2rem auto;
   max-width: 40rem;
   padding: 1rem;
+}
+
+.card:hover {
+  box-shadow: 0 3px 8px rgba(0, 0, 0, 0.2);
 }
 </style>
