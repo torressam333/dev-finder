ea8c26d4c42531528da3ba29ad056bd53bff0ea6
Samuel <torressam333@gmail.com>
2022-02-14 07:25:25 -0700

    Register base button as a global component

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import router from './router';
 import App from './App.vue';
 import store from './store/store';
 import BaseCard from './components/base-components/BaseCard.vue';
+import BaseButton from './components/base-components/BaseButton.vue';
 
 const app = createApp(App);
 
 //I want this globally available
 app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
 
 app.use(router).use(store).mount('#app');
