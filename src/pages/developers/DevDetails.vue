7110c80026b752aae371b35f3153ce68e21f04a8
Samuel <torressam333@gmail.com>
2022-02-19 07:32:57 -0700

    Add templating required for specific dev details component

--- src/pages/developers/DevDetails.vue
+++ src/pages/developers/DevDetails.vue
@@ -1,9 +1,28 @@
 <template>
-  Developer Details Page
-  <!-- This nested router view is needed since this component
-        has child routes    
-     -->
-  <router-view></router-view>
-  <base-button link to="/developers/1/contact">Contact Dev</base-button>
-  <base-button link to="/developers/1/contact">Contact Dev</base-button>
+  <section>
+    <base-card>
+      <!-- Slot data -->
+      <h2>{{ devFullName }}</h2>
+      <h3>${{ devRate }} per hour</h3>
+    </base-card>
+  </section>
+  <section>
+    <header>
+      <h2>Want to connect? Reach out now!</h2>
+      <base-button link :to="contactDevLink">Contact</base-button>
+    </header>
+    <!-- router link required for child form (contact form) to be rendered -->
+    <router-view></router-view>
+  </section>
+  <section>
+    <base-card>
+      <base-badge
+        v-for="area in areas"
+        :key="area"
+        :specialty="area"
+        :title="area"
+      ></base-badge>
+      <p>{{ devDescription }}</p>
+    </base-card>
+  </section>
 </template>
