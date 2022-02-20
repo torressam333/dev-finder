f7f7604362c20fbc7916298239f925c115e5642b
Samuel <torressam333@gmail.com>
2022-02-20 09:13:30 -0700

    Add placeholder unit test

---
+++ tests/unit/DeveloperItem.test.js
@@ -0,0 +1,7 @@
+import { mount } from '@vue/test-utils';
+
+describe('Should contain all information pertinent to a developer', () => {
+  test('placeholder', () => {
+    console.log('some placeholder');
+  });
+});
