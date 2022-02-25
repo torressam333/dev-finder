0443cfedb7b8f9c6ef843b221965f77d965fbfb6
Samuel <torressam333@gmail.com>
2022-02-25 06:31:45 -0700

    Extract wrapper declaration into global test variable

--- tests/unit/DevFilter.spec.js
+++ tests/unit/DevFilter.spec.js
@@ -2,12 +2,17 @@ import { mount } from '@vue/test-utils';
 import DevFilter from '../../src/components/developers/DevFilter.vue';
 
 describe('Should properly filter developers', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(DevFilter);
+  });
+
   test('should properly render a vue instance', () => {
     expect(mount(DevFilter).exists()).toBeTruthy();
   });
 
   test('should have multiple spans with the same class', () => {
-    const wrapper = mount(DevFilter);
     const spans = wrapper.findAll('span');
 
     expect(spans.length).toBe(3);
@@ -19,5 +24,5 @@ describe('Should properly filter developers', () => {
     expect(spans.at(2).classes()).toContain('filter-option');
   });
 
-  test('should change')
+  test('should contain expected data properties', () => {});
 });
