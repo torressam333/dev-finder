b95055d0aa4414424d3b1088c8906747b9038bbd
Samuel <torressam333@gmail.com>
2022-03-16 06:00:15 -0700

    Create template for contact dev form

--- src/components/base-components/BaseButton.vue
+++ src/components/base-components/BaseButton.vue
@@ -9,6 +9,7 @@
 
 <script>
 export default {
+  name: 'base-button',
   props: {
     link: {
       type: Boolean,
--- src/pages/requests/ContactDev.vue
+++ src/pages/requests/ContactDev.vue
@@ -1,1 +1,26 @@
-<template>Contact form for developer</template>
+<template>
+  <form action="">
+    <div>
+      <label for="email">Email</label>
+      <input type="text" id="email" />
+    </div>
+    <div>
+      <label for="message">Message</label>
+      <textarea name="message" id="message" rows="7"></textarea>
+    </div>
+    <div class="actions">
+      <base-button></base-button>
+    </div>
+  </form>
+</template>
+
+<script>
+import BaseButton from '../../components/base-components/BaseButton.vue';
+export default {
+  components: {
+    BaseButton,
+  },
+};
+</script>
+
+<style scoped></style>
