e2b31fdef25ca56ca2de47d597bf00587840d524
Samuel <torressam333@gmail.com>
2022-03-16 06:31:14 -0700

    Add vuex action for new request

---
+++ src/store/modules/requests/actions.js
@@ -0,0 +1,14 @@
+export default {
+  contactDev(context, payload) {
+    const newRequest = {
+      id: new Date().toDateString(), //placeholder to create unique id per request
+      devId: payload.devId,
+      userEmail: payload.email,
+      message: payload.message,
+    };
+
+    //Call add request mutation and pass payload to state when
+    // this action is dispatched
+    context.commit('addRequest', newRequest);
+  },
+};
