af56572e8808ea607f3f2680de1155653fc632b3
Samuel <torressam333@gmail.com>
2022-03-20 09:46:46 -0700

    Create modular request item component to be used

---
+++ src/components/requests/RequestItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <li>
+    <div>
+      <a :href="emailLink">{{ email }}</a>
+    </div>
+    <p>{{ message }}</p>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'request-item',
+  props: {
+    email: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    emailLink() {
+      //open default email client
+      return `mailto:${this.email}`;
+    },
+  },
+};
+</script>
