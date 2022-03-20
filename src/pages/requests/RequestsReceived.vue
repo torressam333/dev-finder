45919f3f291678144c2e889f439d94946123c125
Samuel <torressam333@gmail.com>
2022-03-20 09:36:05 -0700

    Set up outline for requests received component

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,1 +1,15 @@
-<template>REQUESTS COMPONENT</template>
+<template>
+  <section>
+    <base-card>
+      <header><h2>Received Requests</h2></header>
+      <ul></ul>
+      <h3>No Requests Received</h3>
+    </base-card>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
