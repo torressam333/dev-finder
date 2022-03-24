b4433f20b110407f92266406b7e8e5256a4cd7ce
Samuel <torressam333@gmail.com>
2022-03-24 06:04:00 -0700

    Make api call to firebase db to post dev data

--- src/store/modules/developers/actions.js
+++ src/store/modules/developers/actions.js
@@ -1,8 +1,9 @@
 export default {
-  registerDev(context, data) {
+  async registerDev(context, data) {
     //construct dev data - keys must match what the vuex store has
+    const userId = context.rootGetters.userId;
+
     const devData = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
@@ -10,7 +11,28 @@ export default {
       areas: data.areas,
     };
 
+    //Send this to firebase
+    const response = await fetch(
+      `https://dev-finder-d2956-default-rtdb.firebaseio.com/developers/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(devData),
+      }
+    );
+
+    //Returns promise
+    const result = await response.json();
+    console.log(result);
+
+    if (!response.ok) {
+      throw Error('Something went wrong');
+    }
+
     //commit this action w/ newly formatted data keys
-    context.commit('registerDev', devData);
+    context.commit('registerDev', {
+      //Spread original dev data (add in user id after the fact since we want it in vuex)
+      ...devData,
+      id: userId,
+    });
   },
 };
