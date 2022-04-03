f2e3eddc82a94ae24b4103c887c79accc8de31ea
Samuel <torressam333@gmail.com>
2022-04-03 15:32:36 -0700

    Create and user nicer spinner for loading content

--- src/components/base-components/BaseSpinner.vue
+++ src/components/base-components/BaseSpinner.vue
@@ -1,75 +1,26 @@
 <template>
-  <div class="spinner">
-    <div class="lds-roller">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
-  </div>
+  <div class="loading"></div>
 </template>
 
-<style>
-.spinner {
+<style scoped>
+.loading {
+  animation: loader 2s ease infinite;
   display: flex;
   justify-content: center;
-  align-items: center;
-  width: 100%;
 }
 
-.lds-roller {
-  display: inline-block;
-  position: relative;
-  width: 80px;
-  height: 80px;
-}
-.lds-roller div {
-  animation: lds-roller 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
-  transform-origin: 40px 40px;
-}
-.lds-roller div:after {
-  content: ' ';
-  display: block;
-  position: absolute;
-  width: 7px;
-  height: 7px;
+.loading::after {
+  border: 9px solid #dddddd;
   border-radius: 50%;
-  background: #00658d;
-  margin: -4px 0 0 -4px;
+  border-top-color: #00658d;
+  content: '';
+  height: 50px;
+  width: 50px;
 }
-.lds-roller div:nth-child(1) {
-  animation-delay: -0.036s;
-}
-.lds-roller div:nth-child(1):after {
-  top: 63px;
-  left: 63px;
-}
-.lds-roller div:nth-child(2) {
-  animation-delay: -0.072s;
-}
-.lds-roller div:nth-child(2):after {
-  top: 68px;
-  left: 56px;
-}
-.lds-roller div:nth-child(3) {
-  animation-delay: -0.108s;
-}
-.lds-roller div:nth-child(3):after {
-  top: 71px;
-  left: 48px;
-}
-.lds-roller div:nth-child(4) {
-  animation-delay: -0.144s;
-}
-.lds-roller div:nth-child(4):after {
-  top: 72px;
-  left: 40px;
-}
-.lds-roller div:nth-child(5) {
-  animation-delay: -0.18s;
+
+@keyframes loader {
+  to {
+    transform: rotate(2turn);
+  }
 }
 </style>
